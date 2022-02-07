b9ab3d16af810fb7f5a8d17e1dd4c6dab7f0b245
Ronak <96277743+Ronak1414@users.noreply.github.com>
2022-02-07 09:43:38 +0000

    p3 done !!

---
+++ p3original.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int input()
+{
+  int n;
+  printf("enter the number:");
+  scanf("%d",&n);
+  return n;
+}
+
+int composite(int n)
+{ 
+  for (int i=2;i<n/2;i++)
+  {
+    if (n/i != 1)
+    {
+      return 1; 
+    }
+  } 
+return -1; 
+}
+
+
+void output(int n,int c)
+{
+  if(c>0)
+  {
+    printf("%d is a composite",n);
+  }
+  else 
+  printf("%d is not composite",n);
+}
+
+int main()
+{
+int n,c;
+n=input();
+c=composite(n);
+output(n,c);
+return 0;
+}
